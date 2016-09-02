bac106faaf749b6108aed204e1fae977455bc8d6
Linda Gibson <lgibson@numedics.com>
2016-09-02 14:23:55 -0700

    Add and complete the Device and Meter Reading Header class and mapping
    Related Work Items: #2248, #2255, #2256, #2257

---
+++ FirebirdSqlMigration/NuLibrary.Migration/Mappings/TableMappings/DeviceMeterReadingHeaderMapping.cs
@@ -0,0 +1,68 @@
+﻿using NuLibrary.Migration.FBDatabase.FBTables;
+using NuLibrary.Migration.SQLDatabase.EF;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NuLibrary.Migration.Mappings.TableMappings
+{
+    public class PatientDevicesMapping : BaseMapping
+    {
+        /// <summary>
+        /// Default constructor that passes Firebird Table name to base class
+        /// </summary>
+        public PatientDevicesMapping() : base("METERREADERHEADER")
+        {
+
+        }
+
+        public PatientDevicesMapping(string tableName) : base(tableName)
+        {
+
+        }
+
+        public void CreatePatientDevicesMapping()
+        {
+            MappingUtilities mu = new MappingUtilities();
+
+            foreach (DataRow row in TableAgent.DataSet.Tables[FbTableName].Rows)
+            {
+                var dev = new PatientDevice
+                {
+                    PatientId = (String)row["PATIENTID"],
+                    MeterIndex = (int)row["NUMEDICSMETERINDEX"],
+                    Manufacturer = (String)row["MANUFACTURER"],
+                    DeviceModel = (String)row["METERMODEL"],
+                    DeviceName = (String)row["METERNAME"],
+                    SerialNumber = (String)row["SERIALNUMBER"],
+                    SoftwareVersion = (String)row["SOFTWAREVERSION"],
+                    HardwareVersion = (String)row["HARDWAREVERSION"]
+                };
+
+                var mrh = new MeterReadingHeader
+                {
+                    PatientId = (String)row["PATIENTID"],
+                    DownloadKeyId = (int)row["DOWNLOADKEYID"],
+                    DeviceId = (int)row[dev.DeviceId],
+                    ServerDateTime = (DateTime)row["SERVERDATETIME"],
+                    MeterDateTime = (DateTime)row["METERDATETIME"],
+                    Readings = (int)row["READINGS"],
+                    SiteSource = (String)row["SOURCE"],
+                    ReviewedOn = (DateTime)row["REVIEWEDON"]
+                };
+
+
+
+                var pat = mu.FindPatient(dev.PatientId);
+                pat.PatientDevices.Add(dev);
+
+                TransactionManager.DatabaseContext.PatientDevices.Add(dev);
+                TransactionManager.DatabaseContext.MeterReadingHeaders.Add(mrh);
+                TransactionManager.DatabaseContext.Patients.Add(pat);
+            }
+        }
+    }
+}

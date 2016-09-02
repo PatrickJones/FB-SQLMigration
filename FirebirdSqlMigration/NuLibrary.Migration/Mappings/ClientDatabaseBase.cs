df81001ceab6b82b344ee4f6c8d4788577722d45
Linda Gibson <lgibson@numedics.com>
2016-09-02 11:08:58 -0700

    Create ClientDatabaseBase class to help with Disposal in Mapping Utilities
    Related Work Items: #2248, #2252, #2254

---
+++ FirebirdSqlMigration/NuLibrary.Migration/Mappings/ClientDatabaseBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NuLibrary.Migration.Mappings
+{
+    public class ClientDatabaseBase : IDisposable
+    {
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                Dispose();
+            }
+        }
+
+        public void Dispose()
+        {
+
+            GC.SuppressFinalize(this);
+            //this.Dispose();
+        }
+    }
+}
